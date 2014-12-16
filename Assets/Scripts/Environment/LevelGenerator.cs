424f69e5f0d0e64737149920ebccf207c7ffdc7a
ss <Icelight@gmail.com>
2014-12-16 14:37:05 -0600

    Added concept of room difficulty

--- Assets/Scripts/Environment/LevelGenerator.cs
+++ Assets/Scripts/Environment/LevelGenerator.cs
@@ -1,13 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour {
-
-	public GameObject[] pieces;
-
+	
+	public GameObject[] rooms;
+	
+	private Dictionary<int, List<GameObject>> difficultyMap;
+	
+	void Awake() {
+		difficultyMap = new Dictionary<int, List<GameObject>>(8);
+		
+		//Loop through our rooms and build up our difficulty map.
+		foreach (var room in rooms) {
+			var roomInfo = room.GetComponent<RoomInfo>();
+			
+			if (!difficultyMap.ContainsKey(roomInfo.difficulty)) {
+				difficultyMap.Add(roomInfo.difficulty, new List<GameObject>(8));
+			} 
+			
+			difficultyMap[roomInfo.difficulty].Add(room);
+		}
+	}
+	
 	public void GenerateNextRoom() {
-		int pieceIndex = Mathf.FloorToInt(Random.Range(0, pieces.Length));
-		Instantiate (pieces[pieceIndex], new Vector3(this.transform.position.x, this.transform.position.y, 0f),
+		int pieceIndex = Mathf.FloorToInt(Random.Range(0, rooms.Length));
+		Instantiate (rooms[pieceIndex], new Vector3(this.transform.position.x, this.transform.position.y, 0f),
 		             Quaternion.identity);
 	}
 }
---
+++ Assets/Scripts/Environment/RoomInfo.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class RoomInfo : MonoBehaviour {
+
+	public int difficulty;
+}
