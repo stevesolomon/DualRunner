c6c7783003c6badd17e06e13112b01f0f9ebb277
ss <Icelight@gmail.com>
2014-12-18 16:24:07 -0600

    Added basic score keeping

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public Text ScoreText;
+
+	private int score = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		score += (int) (Time.deltaTime * 100f);
+
+		ScoreText.text = score.ToString();
+	}
+}
