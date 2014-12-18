29a34039ceac2f3123dd4be666103d0e7ea6d28e
ss <Icelight@gmail.com>
2014-12-18 15:33:32 -0600

    Added basic hazard

---
+++ Assets/Scripts/Environment/Hazard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazard : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+
+		//TODO: Clean this up - this component should only be responsible for signaling this event :)
+		if (collider.gameObject.CompareTag("Player")) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
