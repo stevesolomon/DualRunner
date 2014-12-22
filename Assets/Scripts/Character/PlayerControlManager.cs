cfc4f99983367e197ec63985c7fd5f14ccc6e9a1
ss <Icelight@gmail.com>
2014-12-22 10:51:29 -0600

    Added touch support for both characters

--- Assets/Scripts/Character/PlayerControlManager.cs
+++ Assets/Scripts/Character/PlayerControlManager.cs
@@ -5,6 +5,8 @@ using UnitySampleAssets.CrossPlatformInput;
 public class PlayerControlManager : MonoBehaviour
 {
 	public string jumpInputName;
+	public Camera controllingCamera;
+
 	private RunnerController character;
 	private bool jumpPressed;
 	private bool jumpExtending;
@@ -19,18 +21,34 @@ public class PlayerControlManager : MonoBehaviour
 		if (!jumpPressed) 
 		{
 			jumpPressed = Input.GetButtonDown(jumpInputName);
-
-			if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
-				jumpPressed = true;
-			}
+			jumpPressed |= CheckTouchJump();
 		}
 
 		jumpExtending = Input.GetButton(jumpInputName);
+		jumpExtending |= CheckTouchExtendJump();
+	}
 
-		if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Stationary ||
-		    Input.GetTouch(0).phase == TouchPhase.Moved)) {
-			jumpExtending = true;
+	//Check all the touches and grab the first one beginning in the range of this camera.
+	private bool CheckTouchJump() {
+		for (var i = 0; i < Input.touchCount; i++) {
+			if (Input.GetTouch(i).phase == TouchPhase.Began &&
+			    controllingCamera.pixelRect.Contains(Input.GetTouch(i).position)) {
+				return true;
+			}
 		}
+
+		return false;
+	}
+
+	private bool CheckTouchExtendJump() {
+		for (var i = 0; i < Input.touchCount; i++) {
+			if ((Input.GetTouch(i).phase == TouchPhase.Stationary || Input.GetTouch(i).phase == TouchPhase.Moved) && 
+			    controllingCamera.pixelRect.Contains(Input.GetTouch(i).position)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private void FixedUpdate()
