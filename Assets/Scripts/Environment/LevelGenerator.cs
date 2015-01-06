0017fbc6bfd204ff50c52e412dab8e5200c323f6
ss <Icelight@gmail.com>
2015-01-05 21:34:25 -0600

    Added padding to level generation to increase difficulty and decrease probability that rooms line up evenly between each screen

--- Assets/Scripts/Environment/LevelGenerator.cs
+++ Assets/Scripts/Environment/LevelGenerator.cs
@@ -16,39 +16,62 @@ public class LevelGenerator : MonoBehaviour {
 
     public int initialXPosition = 0;
 
+    private int nextBlockPositionX;
+    private int nextBlockPositionY;
+
+    public GameObject[] basicBlockPrefabs;
+
 	void Start() {
         if (roomManager == null)
         {
             roomManager = GameObject.Find("RoomManager").GetComponent<RoomManager>();
         }
+
+        nextBlockPositionX = initialXPosition;
+        nextBlockPositionY = roomYPosition;
 	}
 	
 	public void GenerateNextRoom() {        
         var roomRef = roomManager.GetRandomRoom();
 
-        InstantiateNewRoom(roomRef);			
+        InstantiateNewRoom(roomRef, true);			
 	}
 
     public void GenerateStartingRooms()
     {
         var roomRef = roomManager.GetRoomWithDifficulty(0);
 
-        InstantiateNewRoom(roomRef);
-        InstantiateNewRoom(roomRef);
+        InstantiateNewRoom(roomRef, false);
+        InstantiateNewRoom(roomRef, false);
     }
 
-    private Vector2 GetNextRoomPosition()
+    private Vector3 GetNextRoomPosition()
     {
-        int xPosition = initialXPosition + (currRoom * roomLength);
-        int yPosition = roomYPosition;
+        int xPosition = nextBlockPositionX;
+        int yPosition = nextBlockPositionY;
 
-        return new Vector2((float) xPosition, (float) yPosition);
+        nextBlockPositionX += roomLength;
+        
+        return new Vector3((float) xPosition, (float) yPosition, 0f);
     }
 
-    private void InstantiateNewRoom(GameObject roomReference)
+    private void InstantiateNewRoom(GameObject roomReference, bool usePaddingBlocks)
     {
-        var roomPos = GetNextRoomPosition();
-        var newRoom = Instantiate(roomReference, new Vector3(roomPos.x, roomPos.y, 0f), Quaternion.identity) as GameObject;	
+        //Pick a random number of extra ground blocks to add as random spacing
+        var randomSpacingIndex = Mathf.FloorToInt(Random.Range(0.0f, basicBlockPrefabs.Length + 1));
+
+        var roomStartingPos = GetNextRoomPosition();
+        var roomPaddedPos = roomStartingPos;
+
+        if (usePaddingBlocks && randomSpacingIndex != basicBlockPrefabs.Length)
+        {
+            var paddingAmount = randomSpacingIndex + 1;
+            roomPaddedPos = new Vector3(roomPaddedPos.x + paddingAmount, roomPaddedPos.y, roomPaddedPos.z);
+            Instantiate(basicBlockPrefabs[randomSpacingIndex], roomStartingPos, Quaternion.identity);
+            nextBlockPositionX += paddingAmount;
+        }
+
+        var newRoom = Instantiate(roomReference, roomPaddedPos, Quaternion.identity) as GameObject;	
 
         newRoom.transform.FindChild("EnterRoomTrigger").GetComponent<EnterRoom>().levelGenerator = this;
         newRoom.SetActive(true);
