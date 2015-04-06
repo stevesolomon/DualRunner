e4755f20583db6b52f48750e24ca9ddf566cdab0
ss <Icelight@gmail.com>
2015-04-06 14:25:55 -0500

    Posting to twitter now handles login and tweeting in one go

--- Assets/Scripts/Social/SocialManager.cs
+++ Assets/Scripts/Social/SocialManager.cs
@@ -13,13 +13,15 @@ public class SocialManager : MonoBehaviour, IListener<TweetScoreMessage> {
 	void Start () {        
         SoomlaProfile.Initialize();
         ProfileEvents.OnScreenshotCaptured += OnScreenshotCaptured;
+        ProfileEvents.OnLoginFailed += OnLoginFailed;
+        ProfileEvents.OnLoginFinished += OnLoginFinished;
         MessageBus.Instance.Subscribe<TweetScoreMessage>(this);
 
         if (restartCanvas == null)
         {
             restartCanvas = GameObject.Find("RestartPanel").GetComponent<CanvasGroup>();
         }
-	}    
+	}       
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,10 +30,10 @@ public class SocialManager : MonoBehaviour, IListener<TweetScoreMessage> {
 
     public void HandleMessage(TweetScoreMessage message)
     {
-        TweetScore();
+        StartTweetScore();
     }
 
-    public void TweetScore()
+    public void StartTweetScore()
     {
         if (!SoomlaProfile.IsLoggedIn(Provider.TWITTER))
         {
@@ -39,11 +41,34 @@ public class SocialManager : MonoBehaviour, IListener<TweetScoreMessage> {
         }
         else
         {
-            oldCanvasAlpha = restartCanvas.alpha;
-            restartCanvas.alpha = 0f;
-            SoomlaProfile.UploadCurrentScreenShot(this, Provider.TWITTER, "I just got a high scorein #DualRunner!", "Check out my high score in #DualRunner!");            
+            TweetScore();        
         }
     }
+
+    //If not logged in:
+    //   - Log in, subscribe to onLogin events.
+    //   - If successful, tweet score
+    //Else tweet score
+
+    private void TweetScore()
+    {
+        oldCanvasAlpha = restartCanvas.alpha;
+        restartCanvas.alpha = 0f;
+        SoomlaProfile.UploadCurrentScreenShot(this, Provider.TWITTER, "I just got a high scorein #DualRunner!", "Check out my high score in #DualRunner!");    
+    }
+
+    private void OnLoginFinished(UserProfile userProfile, string payload)
+    {
+        if (userProfile.Provider == Provider.TWITTER)
+        {
+            TweetScore();
+        }
+    }
+
+    private void OnLoginFailed(Provider provider, string arg2, string arg3)
+    {
+        //TODO: Add small UI element that says login failed.
+    } 
 
     private void OnScreenshotCaptured(Provider arg1, string arg2)
     {
