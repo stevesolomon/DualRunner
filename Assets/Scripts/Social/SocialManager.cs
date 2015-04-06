3d0af646c62adeb9fc703dfac84103b6d0fc005e
ss <Icelight@gmail.com>
2015-04-05 20:47:30 -0500

    Initial commit to hide restart panel when taking a screenshot. Animation needs fixing

--- Assets/Scripts/Social/SocialManager.cs
+++ Assets/Scripts/Social/SocialManager.cs
@@ -5,11 +5,21 @@ using Assets.Scripts.EventAggregator.Messages;
 
 public class SocialManager : MonoBehaviour, IListener<TweetScoreMessage> {
 
+    public CanvasGroup restartCanvas;
+
+    private float oldCanvasAlpha;
+
 	// Use this for initialization
 	void Start () {        
         SoomlaProfile.Initialize();
+        ProfileEvents.OnScreenshotCaptured += OnScreenshotCaptured;
         MessageBus.Instance.Subscribe<TweetScoreMessage>(this);
-	} 
+
+        if (restartCanvas == null)
+        {
+            restartCanvas = GameObject.Find("RestartPanel").GetComponent<CanvasGroup>();
+        }
+	}    
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,13 +33,23 @@ public class SocialManager : MonoBehaviour, IListener<TweetScoreMessage> {
 
     public void TweetScore()
     {
+        restartCanvas.alpha = 0f;
         if (!SoomlaProfile.IsLoggedIn(Provider.TWITTER))
         {
             SoomlaProfile.Login(Provider.TWITTER);
         }
         else
         {
-            SoomlaProfile.UpdateStory(Provider.TWITTER, "I just got a high score in #DualRunner!", "#DualRunner high score", "My high score", "dual_runner_score", "http://twitter.com", null, null);
+            //SoomlaProfile.UpdateStory(Provider.TWITTER, "I just got a high score in #DualRunner!", "#DualRunner high score", "My high score", "dual_runner_score", "http://twitter.com", null, null);
+            oldCanvasAlpha = restartCanvas.alpha;
+            restartCanvas.alpha = 0f;
+            SoomlaProfile.UploadCurrentScreenShot(this, Provider.TWITTER, "I just got a high scorein #DualRunner!", "Check out my high score in #DualRunner!");
+            
         }
     }
+
+    private void OnScreenshotCaptured(Provider arg1, string arg2)
+    {
+        restartCanvas.alpha = oldCanvasAlpha;
+    } 
 }
