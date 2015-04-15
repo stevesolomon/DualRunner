91564ff91b293bacfaec3f136ef76e26f180f7ed
ss <Icelight@gmail.com>
2015-04-15 15:20:55 -0500

    Fixed issue with a few rooms

--- Assets/Resources/Tilesets/Main.tsx
+++ Assets/Resources/Tilesets/Main.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Main" tilewidth="32" tileheight="32">
- <image source="../../../Sprites/MapSpriteSheet.png" trans="ff00ff" width="128" height="128"/>
+ <image source="../../Sprites/MapSpriteSheet.png" trans="ff00ff" width="128" height="128"/>
  <tile id="0">
   <properties>
    <property name="prefab" value="ground"/>
